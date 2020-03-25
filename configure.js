cb6ddf437ab72b136c3b83d75c870051f94ef6b3
narendra66 <narendra@navanatech.in>
2020-03-25 11:24:34 +0530

    first commit

---
+++ configure.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "app": {
+        "port": 3000
+    },
+    "db": ""
+}
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const config = require('./configure');
+const app = express();  // create object for express;
+
+const port = process.env.port || config.app.port;
+// middlewares define here
+
+
+// routes for server
+app.get("/",(req,res)=>{
+        res.send("server is running!!!");
+})
+// listning server port
+app.listen(port,()=>{
+    console.log("server is running on port:",port);
+})
